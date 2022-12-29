cdb34c4b518fe38c0a845e5a66a71f12d0cdaa40
Marvin Chinchilla <mchinchilla@gmail.com>
2022-12-28 18:50:30 -0600

    Indent and clean code

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,9 +1,9 @@
 
-
 import jwt from 'jsonwebtoken';
 
 
 export const signToken = (_id: string, email: string ) => {
+
     if( !process.env.JWT_SECRET_SEED ) {
         throw new Error('JWT_SECRET_SEED no esta definida, revisar variables de entorno');
     }
@@ -16,7 +16,6 @@ export const signToken = (_id: string, email: string ) => {
         // opciones
         { expiresIn: '30d' },
     );
-
 }
 
 
